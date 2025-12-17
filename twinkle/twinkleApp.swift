d625ae9d6ceec01ac8f738f3ee64daf4048f2258
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-12-17 22:07:37 +0600

    Initial Commit

---
+++ twinkle/twinkleApp.swift
@@ -0,0 +1,17 @@
+//
+//  twinkleApp.swift
+//  twinkle
+//
+//  Created by farhan on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct twinkleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
